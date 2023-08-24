cc8652889d1ec52c7c8df84bbb7c26da8ed4553b
MazeL98 <cherik0908@gmail.com>
2023-08-25 04:14:31 +0800

    chore: fix the bug npm run build freezes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
-
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack(config) {
@@ -11,11 +11,21 @@ module.exports = defineConfig({
       })
     )
     config.plugin('circular').use(new CircularDependencyPlugin())
-    if (process.env.use_analyzer) {
-      config
-        .plugin('webpack-bundle-analyzer')
-        .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
-        .end()
-    }
+    config.plugin('compression').use(
+      new CompressionWebpackPlugin({
+        filename: '[path][base].gz',
+        algorithm: 'gzip',
+        test: /\.(js|css|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+        deleteOriginalAssets: false
+      })
+    )
+    // if (process.env.use_analyzer) {
+    //   config
+    //     .plugin('webpack-bundle-analyzer')
+    //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    //     .end()
+    // }
   }
 })
